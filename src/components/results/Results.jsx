ddd33559b6b2f90a6594123c28faed572de0c094
Alan Heizenreder <alanheizenreder01@gmail.com>
2024-04-14 06:28:05 -0300

    bug fixed

--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -2,26 +2,29 @@
 
 export const Results = ({ num1, num2, sign, result, onReset }) => {
   return (
-    <>
-      <input
-        type="text"
-        className="screen"
-        value={`${num1} ${sign} ${num2}`}
-        readOnly
-      />
+    <div class=" d-flex align-items-center">
+      <div class="row w-100">
+        <div class="col-12 col-sm-9 col-md-7 col-lg-5 mx-auto">
+          <input
+            type="text"
+            className="screen"
+            value={`${num1} ${sign} ${num2}`}
+            readOnly
+          />
 
-      <input
-        type="text"
-        className="result"
-        value={result !== null ? result : ""}
-        readOnly
-      />
-
-      <div className="ac-container">
-        <button className="btn btn-sm btn-dark" onClick={onReset}>
-          AC
-        </button>
+          <input
+            type="text"
+            className="result"
+            value={result !== null ? result : ""}
+            readOnly
+          />
+          <div className="ac-container">
+            <button className="btn btn-sm btn-dark" onClick={onReset}>
+              AC
+            </button>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
